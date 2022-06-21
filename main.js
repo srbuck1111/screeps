4a846467b51d00c857ff0401738b3bfaa6d7b586
Sean <srbuck1111@gmail.com>
2022-06-21 02:11:11 -0400

    added builder role to main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
 var spawnHelper = require('help.spawns');
 
 module.exports.loop = function () {
