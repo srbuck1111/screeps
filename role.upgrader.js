03998048c19f2169ee9b313e7e7d4609758defac
MrFaceguy <58958252+MrFaceguy@users.noreply.github.com>
2022-06-21 23:58:34 -0400

    fixed upgrader code

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,9 +1,16 @@
 var roleUpgrader = {
-
+    
     /** @param {Creep} creep **/
     run: function(creep) {
+
 	    if(creep.store[RESOURCE_ENERGY] == 0) {
-            var sources = creep.room.find(FIND_SOURCES);
+            creep.memory.isFull = false;
+	    }
+	    else if(creep.store[RESOURCE_ENERGY] == creep.store.getCapacity()){
+	        creep.memory.isFull = true;
+	    }
+        if(creep.memory.isFull == false){
+            var sources = creep.room.find(FIND_SOURCES_ACTIVE);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
             }
