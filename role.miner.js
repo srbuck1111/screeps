cb4b498ef09f2303c021e3a555a19367c4ba88b6
Sean <srbuck1111@gmail.com>
2022-06-27 08:09:48 -0400

    finished container miner role, creates containers if not present

--- role.miner.js
+++ role.miner.js
@@ -3,6 +3,7 @@ var roleMiner = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
+        //Decides whether the creep should be mining or not
 	    if(!creep.memory.mining && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.mining = true;
             creep.say('⛏ mine');
@@ -12,16 +13,20 @@ var roleMiner = {
 	        creep.say('📦 store');
 	    }
 
+        //If mining check if near a node and if so mine it.
 	    if(creep.memory.mining) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
+
+        //If finished mining deposit into container
         else {
+            //Check for a container directly below itself to deposit into
             var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_STORAGE) &&
+                        return structure.structureType == STRUCTURE_CONTAINER &&
                             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 &&
                             creep.pos.getRangeTo(structure) <= 1;
                     }
@@ -30,17 +35,19 @@ var roleMiner = {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
-            } else {
+            } 
+            //If no container is found, place one or continue constructing it
+            else {
                 var targets = creep.room.find(FIND_CONSTRUCTION_SITES, {
                     filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_STORAGE) &&
+                        return structure.structureType == STRUCTURE_CONTAINER &&
                             creep.pos.getRangeTo(structure) <= 1;
                     }
                 });
                 if (targets.length > 0) {
                     creep.build(targets[0])
                 } else {
-                    //place container nearby
+                    creep.pos.createConstructionSite(STRUCTURE_CONTAINER);
                 }
             }
         }
